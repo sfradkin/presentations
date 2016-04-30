92cd3209fe91a2e62817ed8fe663e5de73266dc4
Scott Fradkin <scott@fradkin.com>
2016-04-30 17:08:24 -0500

    adding new songs from 04302016 sonic pi workshop, adding WAC 2016 presentation

---
+++ kids-sonic-pi-workshop/songs/04302016/star_wars.rb
@@ -0,0 +1,52 @@
+#r = (ring :c4, :e4, :g4)
+
+#use_random_seed
+
+use_synth :dsaw
+
+play :c4
+sleep 0.3
+play :c4
+sleep 0.3
+play :c4
+sleep 0.3
+play :f4
+sleep 1
+play :c5
+sleep 1
+play 70
+sleep 0.3
+play :a4
+sleep 0.3
+play :g4
+sleep 0.3
+play 77
+sleep 1
+play 72
+sleep 1
+play 70
+sleep 0.3
+play :a4
+sleep 0.3
+play :g4
+sleep 0.3
+play 77
+sleep 1
+play 72
+sleep 1
+play 70
+sleep 0.3
+play 69
+sleep 0.3
+play 70
+sleep 0.3
+play 67
+
+
+
+
+
+#6.times do
+#  play r.choose
+#  sleep 0.5
+#end
