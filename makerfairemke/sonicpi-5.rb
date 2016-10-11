bc07e9e81d90f64ce4e1e25d2471b3b0cae6bebf
Scott Fradkin <scott@fradkin.com>
2016-10-11 17:22:47 -0500

    adding makerfaire milwaukee workshop and presentations

---
+++ makerfairemke/sonicpi-5.rb
@@ -0,0 +1,12 @@
+# Hello MakerFaireMilwaukee!
+# This is file 5 of 5
+# You can find it at
+# https://github.com/sfradkin/presentations/tree/master/makerfairemke
+# after the conference
+
+# some other neat functionality:
+
+# at
+# on
+# spread
+# (buffer 8)
